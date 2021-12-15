2a60eb42cee227b1a1ed49b2b076506137ba6755
iremaslandogan <iremaslandogan@gmail.com>
2021-12-15 17:52:32 +0300

    basic

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? ImagePath { get; set; }
+        public int TownId { get; set; }
+        public Town? Town { get; set; }
+        public int IsActive { get; set; } = 1;
+        public int IsDelete { get; set; } = 0;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ Entities/Concrete/UserDonationType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class UserDonationType
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public int DonorTypeId { get; set; }
+
+        public DonationType? DonationType { get; set; }
+
+        public int IsActive { get; set; } = 1;
+        public int IsDelete { get; set; } = 0;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+
+    }
+}
